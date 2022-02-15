bb69870ce45d5dd971a15ad5ea0ec6ef6b5ca626
noobAbishank <nayakabishank1999@gmail.com>
2022-02-15 12:50:27 +0530

    Set Debug_Value

--- django_project/settings.py
+++ django_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('MYBLOG_DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['my-blog-django.herokuapp.com']
 
